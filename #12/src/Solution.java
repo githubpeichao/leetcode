a31fa19d473d14936fdb68669efe322587e159bf
peichao <kiyotaka_li@163.com>
2019-09-03 20:43:00 +0800

    12

---
+++ #12/src/Solution.java
@@ -0,0 +1,18 @@
+
+public class Solution {
+	    public String intToRoman(int num) {
+	        int[] nums={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+	        String[] s={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+	        StringBuilder sb=new StringBuilder();
+	        int index=0;
+	        while(index<13){
+	            while(num>=nums[index]){
+	                sb.append(s[index]);
+	                num=num-nums[index];
+	            }
+	            index++;
+	        }
+	        return sb.toString();
+	    }
+	
+}
