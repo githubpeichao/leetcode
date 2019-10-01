1dba3818f893b66994a24fba0c8442309a53c51a
githubpeichao <46468426+githubpeichao@users.noreply.github.com>
2019-10-01 22:23:03 +0800

    Update Solution.java

--- #55/src/Solution.java
+++ #55/src/Solution.java
@@ -1,4 +1,22 @@
-/*class Solution {
+/*给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个位置。
+
+示例 1:
+
+输入: [2,3,1,1,4]
+输出: true
+解释: 从位置 0 到 1 跳 1 步, 然后跳 3 步到达最后一个位置。
+示例 2:
+
+输入: [3,2,1,0,4]
+输出: false
+解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+*/
+/*   回溯暴力破解---超时了
+class Solution {
     public boolean canJump(int[] nums) {
         return can(nums,0);
     }
@@ -11,6 +29,7 @@
         return false;
     }
 }*/
+//动态规划-------Boolean类型的dp数组----题解有贪心算法优化---翻翻看
 class Solution {
     public boolean canJump(int[] nums) {
         boolean[] dp=new boolean[nums.length];
