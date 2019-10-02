bfe939fe34b6bcd9fb1b49c2a5a4ef9f712d1997
githubpeichao <46468426+githubpeichao@users.noreply.github.com>
2019-10-02 11:18:40 +0800

    Update Solution.java

--- #46/src/Solution.java
+++ #46/src/Solution.java
@@ -1,9 +1,26 @@
+/*
+给定一个没有重复数字的序列，返回其所有可能的全排列。
+
+示例:
+
+输入: [1,2,3]
+输出:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+*/
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
-
+//回溯法解决，用map记录这个数是否用过，用stack保存顺序，每次回溯都要重置状态，将nums[i]从map中删除
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         Stack<Integer> stack=new Stack<>();
