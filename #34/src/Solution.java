cde3e223774b77c672c9704e6e5abb52037c35c9
githubpeichao <46468426+githubpeichao@users.noreply.github.com>
2019-10-12 17:33:22 +0800

    Update Solution.java

--- #34/src/Solution.java
+++ #34/src/Solution.java
@@ -1,3 +1,22 @@
+/*
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+
+如果数组中不存在目标值，返回 [-1, -1]。
+
+示例 1:
+
+输入: nums = [5,7,7,8,8,10], target = 8
+输出: [3,4]
+示例 2:
+
+输入: nums = [5,7,7,8,8,10], target = 6
+输出: [-1,-1]
+
+看题解吧
+https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/er-fen-cha-zhao-suan-fa-xi-jie-xiang-jie-by-labula/
+*/
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans=new int[2];
@@ -31,7 +50,7 @@ class Solution {
         while (left<right) {
             int mid=(left+right) / 2;
             if(nums[mid]==target) {
-                left=mid+1; // ע��
+                left=mid+1; // ×˘Ňâ
             }else if(nums[mid]<target) {
                 left=mid+1;
             }else if(nums[mid]>target) {
