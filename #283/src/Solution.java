29333010b4f17135274f17eaadaf0ac6c5b1d918
githubpeichao <46468426+githubpeichao@users.noreply.github.com>
2019-10-22 18:35:59 +0800

    Update Solution.java

--- #283/src/Solution.java
+++ #283/src/Solution.java
@@ -1,3 +1,13 @@
+/*
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+
+每一个非0数前面有多少个0，就index前移多少，再后面补上0
+*/
 class Solution {
     public void moveZeroes(int[] nums) {
         int zeronum=0;
