2d3b0c7a689350deebde5609069f1a579988334b
githubpeichao <46468426+githubpeichao@users.noreply.github.com>
2019-10-22 18:40:30 +0800

    Update Solution.java

--- #438/src/Solution.java
+++ #438/src/Solution.java
@@ -1,3 +1,26 @@
+/*
+给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+
+字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+
+说明：
+
+字母异位词指字母相同，但排列不同的字符串。
+不考虑答案输出的顺序。
+示例 1:
+
+输入:
+s: "cbaebabacd" p: "abc"
+
+输出:
+[0, 6]
+
+解释:
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
+
+用map存储超时了
+*/
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,12 +40,12 @@ class Solution {
 
         for(int i = 0;i < s_arr.length - p_arr.length + 1;i++){
             char[] temp = new char[26];
-            //����hash
+            //放入hash
             if(p_letter[s_arr[i] - 'a'] == 0) continue;
             for(int j = i; j < i + p_arr.length;j++){
                 temp[s_arr[j] - 'a']++;
             }
-            //ƥ��hash
+            //匹配hash
             boolean ok = true;
             for(int j = 0;j < p_arr.length;j++){
                 if(temp[p_arr[j] - 'a'] == 0){
@@ -32,7 +55,7 @@ class Solution {
                     temp[p_arr[j] - 'a']--;
                 }
             }
-            //ok ����list
+            //ok 加入list
             if(ok){
                 list.add(i);
             }
@@ -41,7 +64,7 @@ class Solution {
 
     }
 
-	/*��map�洢��ʱ��
+    /*用map存储超时了
     public List<Integer> findAnagrams(String s, String p) {
         int length=p.length();
         List<Integer> res=new ArrayList<>();
